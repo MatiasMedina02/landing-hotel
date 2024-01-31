cb8085bd9454628bd950955c47a41ec9035c81f9
MatiasMedina02 <rusomedina02@gmail.com>
2024-01-31 18:05:47 -0300

    Sliders listos

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,7 @@
 import { Box, Card, CardContent, CardMedia, Grid, Stack, Typography } from "@mui/material"
 import { KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material";
 import { lunches, breakfasts, desserts, drinks } from "../helpers/data";
+import Carousel from "react-material-ui-carousel"
 
 const Menu = () => {
   return (
@@ -10,7 +11,33 @@ const Menu = () => {
       {/* Almuerzos */}
       <Box mb={8}>
         <Typography variant="h5" mb={2}>Almuerzos</Typography>
-        <Grid container alignItems="center" spacing={5}>
+        <Box minWidth="300px">
+          <Carousel
+            NextIcon={<KeyboardArrowRight style={{ color: "white", fontSize: "40px" }} />}
+            PrevIcon={<KeyboardArrowLeft style={{ color: "white", fontSize: "40px" }} />}
+            interval={6000}
+            indicators={false}
+            navButtonsAlwaysVisible={true}
+            animation="slide"
+          >
+            {lunches.map(lunch => (
+              <Card key={lunch.name} style={{
+                padding: "16px"
+              }}>
+                <CardMedia
+                  component="img"
+                  image={lunch.img}
+                  alt={lunch.name}
+                />
+                <CardContent>
+                  <Typography textAlign="center" variant="h6">Nombre</Typography>
+                  <Typography textAlign="center" variant="body2">00.00 $</Typography>
+                </CardContent>
+              </Card>
+            ))}
+          </Carousel>
+        </Box>
+        {/* <Grid container alignItems="center" spacing={5}>
           <Grid item>
             <KeyboardArrowLeft fontSize="large" />
           </Grid>
@@ -34,19 +61,23 @@ const Menu = () => {
           <Grid item>
             <KeyboardArrowRight fontSize="large" />
           </Grid>
-        </Grid>
+        </Grid> */}
       </Box>
 
       {/* Desayunos y Cenas */}
       <Box mb={8}>
         <Typography variant="h5" mb={2}>Desayunos y Cenas</Typography>
-        <Grid container alignItems="center" spacing={5}>
-          <Grid item>
-            <KeyboardArrowLeft fontSize="large" />
-          </Grid>
-          {breakfasts.map(breakfast => (
-            <Grid item key={breakfast.name}>
-              <Card style={{
+        <Box minWidth="300px">
+          <Carousel
+            NextIcon={<KeyboardArrowRight style={{ color: "white", fontSize: "40px" }} />}
+            PrevIcon={<KeyboardArrowLeft style={{ color: "white", fontSize: "40px" }} />}
+            interval={6000}
+            indicators={false}
+            navButtonsAlwaysVisible={true}
+            animation="slide"
+          >
+            {breakfasts.map(breakfast => (
+              <Card key={breakfast.name} style={{
                 padding: "16px"
               }}>
                 <CardMedia
@@ -59,24 +90,25 @@ const Menu = () => {
                   <Typography textAlign="center" variant="body2">00.00 $</Typography>
                 </CardContent>
               </Card>
-            </Grid>
-          ))}
-          <Grid item>
-            <KeyboardArrowRight fontSize="large" />
-          </Grid>
-        </Grid>
+            ))}
+          </Carousel>
+        </Box>
       </Box>
 
       {/* Postres */}
       <Box mb={8}>
         <Typography variant="h5" mb={2}>Postres</Typography>
-        <Grid container alignItems="center" spacing={5}>
-          <Grid item>
-            <KeyboardArrowLeft fontSize="large" />
-          </Grid>
-          {desserts.map(dessert => (
-            <Grid item key={dessert.name}>
-              <Card style={{
+        <Box minWidth="300px">
+          <Carousel
+            NextIcon={<KeyboardArrowRight style={{ color: "white", fontSize: "40px" }} />}
+            PrevIcon={<KeyboardArrowLeft style={{ color: "white", fontSize: "40px" }} />}
+            interval={6000}
+            indicators={false}
+            navButtonsAlwaysVisible={true}
+            animation="slide"
+          >
+            {desserts.map(dessert => (
+              <Card key={dessert.name} style={{
                 padding: "16px"
               }}>
                 <CardMedia
@@ -89,42 +121,42 @@ const Menu = () => {
                   <Typography textAlign="center" variant="body2">00.00 $</Typography>
                 </CardContent>
               </Card>
-            </Grid>
-          ))}
-          <Grid item>
-            <KeyboardArrowRight fontSize="large" />
-          </Grid>
-        </Grid>
+            ))}
+          </Carousel>
+        </Box>
       </Box>
 
       {/* Bebidas */}
       <Box mb={8}>
         <Typography variant="h5" mb={2}>Bebidas</Typography>
-        <Grid container alignItems="center" spacing={5}>
-          <Grid item>
-            <KeyboardArrowLeft fontSize="large" />
-          </Grid>
-          {drinks.map(drink => (
-            <Grid item key={drink.name}>
-              <Card style={{
-                padding: "16px"
-              }}>
-                <CardMedia
-                  component="img"
-                  image={drink.img}
-                  alt={drink.name}
-                />
-                <CardContent>
-                  <Typography textAlign="center" variant="h6">Nombre</Typography>
-                  <Typography textAlign="center" variant="body2">00.00 $</Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
-          <Grid item>
-            <KeyboardArrowRight fontSize="large" />
-          </Grid>
-        </Grid>
+        <Box minWidth="300px">
+          <Carousel
+            NextIcon={<KeyboardArrowRight style={{ color: "white", fontSize: "40px" }} />}
+            PrevIcon={<KeyboardArrowLeft style={{ color: "white", fontSize: "40px" }} />}
+            interval={6000}
+            indicators={false}
+            navButtonsAlwaysVisible={true}
+            animation="slide"
+          >
+            {drinks.map(drink => (
+              <Grid item key={drink.name}>
+                <Card key={drink.name} style={{
+                  padding: "16px"
+                }}>
+                  <CardMedia
+                    component="img"
+                    image={drink.img}
+                    alt={drink.name}
+                  />
+                  <CardContent>
+                    <Typography textAlign="center" variant="h6">Nombre</Typography>
+                    <Typography textAlign="center" variant="body2">00.00 $</Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Carousel>
+        </Box>
       </Box>
     </Stack>
   )
--- src/components/Reservations.tsx
+++ src/components/Reservations.tsx
@@ -1,53 +1,71 @@
 import { KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material"
-import { Button, Card, CardContent, CardMedia, Grid, Typography } from "@mui/material"
+import { Box, Button, Card, CardContent, CardMedia, Typography } from "@mui/material"
 import { brown } from "@mui/material/colors"
 import Room1 from "../assets/habitacion1.png"
+import Room2 from "../assets/habitacion2.jpg"
+import Room3 from "../assets/habitacion3.jpg"
+import Carousel from "react-material-ui-carousel";
 
 const Reservations = () => {
+  const rooms = [
+    { name: "Room1", img: Room1 },
+    { name: "Room2", img: Room2 },
+    { name: "Room3", img: Room3 },
+  ]
+
   return (
-    <Grid container mb={8} sx={{
-      bgcolor: brown[300],
-      paddingY: 4,
-      alignItems: "center",
-    }}>
-      <Grid item xs display="flex" justifyContent="center">
-        <KeyboardArrowLeft style={{ color: "white", fontSize: "40px" }} />
-      </Grid>
-      <Grid item xs={8}>
-        <Card sx={{
-          display: "flex",
-          color: "white",
-          bgcolor: "transparent",
-          boxShadow: "none",
-          borderRadius: 0
-        }}>
-          <CardMedia
-            component="img"
-            image={Room1}
-            alt="Habitación"
-          />
-          <CardContent>
-            <Typography variant="h2">Nombre de la habitación</Typography>
-            <Typography variant="body1">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            </Typography>
-            <Typography variant="body1">
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-            </Typography>
-            <Button variant="contained" sx={{
-              bgcolor: "white",
-              color: brown[300],
-              ":hover": {
-                bgcolor: "lightgray"
-              }
-            }}>Hacer Reservación</Button>
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid item xs display="flex" justifyContent="center">
-        <KeyboardArrowRight style={{ color: "white", fontSize: "40px" }} />
-      </Grid>
-    </Grid>
+    <Box
+      sx={{
+        bgcolor: brown[300],
+        paddingY: 4,
+        alignItems: "center",
+        marginBottom: 8
+      }}
+    >
+      <Carousel
+        sx={{ padding: { xs: "20px", sm: "20px 100px", md: "20px 150px" } }}
+        NextIcon={<KeyboardArrowRight style={{ color: "white", fontSize: "40px" }} />}
+        PrevIcon={<KeyboardArrowLeft style={{ color: "white", fontSize: "40px" }} />}
+        interval={6000}
+        indicators={false}
+        navButtonsAlwaysVisible={true}
+      >
+        {rooms.map(room => (
+          <Card key={room.name} sx={{
+            display: { md: "flex" },
+            color: "white",
+            bgcolor: "transparent",
+            boxShadow: "none",
+            borderRadius: 0
+          }}>
+            <CardMedia
+              width="450"
+              height="270"
+              sx={{ borderRadius: "10px" }}
+              component="img"
+              image={room.img}
+              alt={room.name}
+            />
+            <CardContent sx={{ textAlign: { xs: "center", sm: "start" } }}>
+              <Typography mb={2} variant="h2" sx={{ fontWeight: 800, fontSize: { xs: "20px", sm: "32px" } }}>Nombre de la habitación</Typography>
+              <Typography variant="body1">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              </Typography>
+              <Typography mb={2} variant="body1">
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+              </Typography>
+              <Button variant="contained" sx={{
+                bgcolor: "white",
+                color: brown[300],
+                ":hover": {
+                  bgcolor: "lightgray"
+                }
+              }}>Hacer Reservación</Button>
+            </CardContent>
+          </Card>
+        ))}
+      </Carousel>
+    </Box>
   )
 }
 
